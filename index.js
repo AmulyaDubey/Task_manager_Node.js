d55fd6ced2a7f14b7ffc60c1e4efd2e7c813e005
macbookpro <amulyadubey99@gmail.com>
2020-05-02 20:17:34 +0530

    Login and Logout done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -13,3 +14,12 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const jwt=require('jsonwebtoken')
+
+const myFunction = async () => {
+    const token=jwt.sign({ _id:'abc123'}, 'thisismynewcourse')
+    console.log(token)
+}
+
+myFunction()
